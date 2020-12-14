447ed9ecfaf3126e885c4f6bf00f64d9c68905c7
dejan.subasic <dejan.subasic@gmail.com>
2020-12-14 19:02:46 +0100

    Defined Alarm class.

---
+++ Models/Alarm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public enum AlarmMessagesTypes
+    {
+        LowPrio = 0,
+        StandardPrio,
+        HighPrio
+    }
+
+    public class Alarm
+    {
+        public DateTime CreationTime { get; private set; }
+
+        private int _risk;
+
+        public int Risk {
+            get { return _risk; }
+            private set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Risk values are in the scope of [1-100]");
+                }
+                _risk = value;
+            }
+        }
+        public string Message { get; private set; }
+
+        public Alarm(DateTime creation, int risk, AlarmMessagesTypes type)
+        {
+            CreationTime = creation;
+            Risk = risk;
+            ResourceManager rM = new ResourceManager(typeof(AlarmMessages).FullName, Assembly.GetExecutingAssembly());
+            Message = rM.GetString(type.ToString());
+        }
+
+    }
+}
