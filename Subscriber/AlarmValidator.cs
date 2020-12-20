71619aa507e5f366f40b2a9e0b2af82ce059ace3
dejan.subasic <dejan.subasic@gmail.com>
2020-12-20 15:59:30 +0100

    Implemented AlarmValidator

---
+++ Subscriber/AlarmValidator.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    public static class AlarmValidator
+    {
+        static bool Validate(Alarm alarm) //how can an alarm be invalid?
+        {
+            if (alarm.Risk < 0 || alarm.Risk > 100) return false;
+
+            if (alarm.Message == null || alarm.Message == string.Empty)
+            {
+                return true;
+            }
+
+            return true;
+        }
+    }
+}
